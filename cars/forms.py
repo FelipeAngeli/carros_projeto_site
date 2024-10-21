3566a3ec631547efac4d9863aaa009fc8d9a5433
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-10-21 20:32:49 -0300

    ModelForm e validaçao

--- cars/forms.py
+++ cars/forms.py
@@ -1,26 +1,27 @@
 from django import forms 
-from cars.models import Brand, Car
+from cars.models import Car
 
-class CarForm(forms.Form):  
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(queryset=Brand.objects.all())  
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    color = forms.CharField(max_length=50)
-    photo = forms.ImageField()
 
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            color = self.cleaned_data['color'],
-            photo = self.cleaned_data['photo']
-        )
-        car.save() #save no banco de dados
-        return car
+
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__' 
+    
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'O valor do carro deve ser maior que 20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1960:
+            self.add_error('factory_year', 'O ano de fabricação do carro deve ser maior que 1960')
+        return factory_year
+    
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo')
+        if not photo:
+            self.add_error('photo', 'O carro deve ter uma foto')
+        return photo
