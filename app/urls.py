c4ec65dc83b45c85d97a7f6393e184b773978b8c
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-10-22 10:37:15 -0300

    register, login, logout

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view , new_cars_view
 from accounts.views import register_view
+from accounts.views import login_view
+from accounts.views import logout_view
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('cars/',cars_view, name='cars_list'),
     path('new_car/', new_cars_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
